bd4af19ae1a84a3de324ecc1dc2d55702f1d4fde
Gde <gugumgde26@gmail.com>
2025-03-23 13:24:25 +0700

    Menambahkan fitu edit profile yang terhubung dengan data pengguna

--- lib/data/models/profil_pengguna_model.dart
+++ lib/data/models/profil_pengguna_model.dart
@@ -1,34 +1,29 @@
+// profil_pengguna_model.dart
 class ProfilPengguna {
-  int id;
-  int fkIdPengguna;
-  String jenisKelamin;
-  double beratBadan;
-  String jamBangun;
-  String jamTidur;
+  final int id;
+  final int fkIdPengguna;
+  final String jenisKelamin;
+  final double beratBadan;
+  final String jamBangun;
+  final String jamTidur;
 
-  ProfilPengguna(
-      {required this.id,
-      required this.fkIdPengguna,
-      required this.jenisKelamin,
-      required this.beratBadan,
-      required this.jamBangun,
-      required this.jamTidur});
+  ProfilPengguna({
+    required this.id,
+    required this.fkIdPengguna,
+    required this.jenisKelamin,
+    required this.beratBadan,
+    required this.jamBangun,
+    required this.jamTidur,
+  });
 
-  factory ProfilPengguna.fromMap(Map<String, dynamic> map) => ProfilPengguna(
-        id: map['id'],
-        fkIdPengguna: map['fk_id_pengguna'],
-        jenisKelamin: map['jenis_kelamin'],
-        beratBadan: map['berat_badan'],
-        jamBangun: map['jam_bangun'],
-        jamTidur: map['jam_tidur'],
-      );
-
-  Map<String, dynamic> toMap() => {
-        'id': id,
-        'fk_id_pengguna': fkIdPengguna,
-        'jenis_kelamin': jenisKelamin,
-        'berat_badan': beratBadan,
-        'jam_bangun': jamBangun,
-        'jam_tidur': jamTidur,
-      };
+  factory ProfilPengguna.fromMap(Map<String, dynamic> map) {
+    return ProfilPengguna(
+      id: map['id'] as int,
+      fkIdPengguna: map['fk_id_pengguna'] as int,
+      jenisKelamin: map['jenis_kelamin'] as String,
+      beratBadan: map['berat_badan'] as double,
+      jamBangun: map['jam_bangun'] as String,
+      jamTidur: map['jam_tidur'] as String,
+    );
+  }
 }
